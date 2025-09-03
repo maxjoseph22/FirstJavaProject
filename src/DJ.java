d4c5505a9cac0725b9b270d532b05985dc37fc08
Max Joseph <maxjoseph22@gmail.com>
2025-09-03 10:30:53 +0100

    first commit

---
+++ src/DJ.java
@@ -0,0 +1,17 @@
+public class DJ {
+    public String name;
+    public String genre;
+    public int recordCollectionSize;
+    public double fee;
+
+    public DJ(String name, String genre, int recordCollectionSize, double fee) {
+        this.name = name;
+        this.genre = genre;
+        this.recordCollectionSize = recordCollectionSize;
+        this.fee = fee;
+    }
+
+}
+
+
+
